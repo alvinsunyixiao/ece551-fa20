8d8b4cef79d81b56b2b5b1482bda8b2becfc030d
Alvin Sun <alvinsunyixiao@gmail.com>
2020-09-24 18:51:27 -0700

    finish coding

--- hw4/HW4.py
+++ hw4/HW4.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from scipy import ndimage
 
-def H(x, N, spread, noise_power):
+def H(x, N, spread, noise_power, targets=None):
     """
     Takes an input waveform `x`, a number of targets `N`,
     a standard `deviation` for the random placement of
@@ -15,47 +15,105 @@ def H(x, N, spread, noise_power):
     pulse_width = len(x)
 
     out = np.sqrt(noise_power) * np.random.randn(recording_length)
-    targets = np.clip(np.floor(spread * np.random.randn(N) + recording_length/2).astype(int),0,recording_length - pulse_width)
+    if targets is None:
+        targets = np.clip(np.floor(spread * np.random.randn(N) + recording_length/2).astype(int),0,recording_length - pulse_width)
+        targets = sorted(targets)
 
     for target in targets:
         out[target:target+pulse_width] += x
 
     return out, targets
 
-def rectangular_pulse(L,freq):
-    return np.sin(freq*np.arange(L))
-
-def sinc_pulse(L, omega_b):
-    delay = 0
-    n = (np.linspace(-L//2, L//2, L, endpoint=False) - delay) * omega_b / np.pi
-    return np.sinc(n)
 
 # settings
 N = 4
-spread = 300
+spread = 100
 noise_power = 1e-3
+signal_power = 5e-1
 L = 128
-freq = np.pi / 20
+freq = np.pi / 4
 
-def peak_corr(pulse, receiver):
+def expected_power(signal):
+    return np.sum(signal**2) / signal.shape[0]
+
+def power_normalize(signal):
+    return signal / np.sqrt(expected_power(signal) / signal_power)
+
+def rectangular_pulse(L, freq):
+    pulse = np.sin(freq * np.arange(L))
+    return power_normalize(pulse)
+
+def triangular_pulse(L, freq):
+    pulse = np.sin(freq * np.arange(L))
+    window = np.bartlett(L)
+    return power_normalize(pulse * window)
+
+def hamming_pulse(L, freq):
+    pulse = np.sin(freq * np.arange(L))
+    window = np.hamming(L)
+    return power_normalize(pulse * window)
+
+def sinc_pulse(L, omega_b):
+    n = np.linspace(-L//2, L//2, L, endpoint=False)
+    pulse = np.sinc(n * omega_b / np.pi)
+    return power_normalize(pulse)
+
+def warped_sinc_pulse(L, omega_b, num_bands):
+    n = np.arange(L, dtype=float)
+    pulse = np.zeros_like(n)
+    T = L / num_bands # spread out in time as much as possible
+    for m in range(num_bands):
+        n_m = n - m * T
+        w_h = (m + 1) / num_bands * omega_b / np.pi
+        w_l = m / num_bands * omega_b / np.pi
+        pulse += w_h * np.sinc(w_h * n_m) - w_l * np.sinc(w_l * n_m)
+    return power_normalize(pulse)
+
+def peak_corr(pulse, receiver, name):
     corr = np.correlate(receiver, pulse, mode='valid')
     corr_abs = np.abs(corr)
-    corr_max = ndimage.maximum_filter1d(corr_abs, L//2, mode='constant')
-    corr_min = ndimage.minimum_filter1d(corr_abs, L//2, mode='constant')
-    indics_mask = (corr_abs == corr_max) & (corr_max - corr_min >= 10)
+    corr_max = ndimage.maximum_filter1d(corr_abs, L//8, mode='constant')
+    corr_min = ndimage.minimum_filter1d(corr_abs, L//8, mode='constant')
+    indics_mask = (corr_abs == corr_max) & (corr_max - corr_min >= .3*signal_power*L)
     indics = np.argwhere(indics_mask).flatten()
-    plt.figure()
+    plt.figure(figsize=(15, 5))
+    # plot received signal
+    plt.subplot(121)
+    plt.plot(receiver)
+    plt.title(f'{name} pulse receiver')
+    # plot absolute correlation
+    plt.subplot(122)
     plt.plot(corr_abs)
-    plt.title(f'Estimated Delays (via peak finding): {indics}')
+    plt.title(f'absolute correlation of {name} pulse\nestimated delays: {indics}')
 
 if __name__ == '__main__':
+    # rectangle windowed sinusoid
     pulse = rectangular_pulse(L,freq)
-    pulse = sinc_pulse(L, np.pi/20)
-    print(pulse.max())
     receiver, delays = H(pulse,N,spread,noise_power)
-    plt.figure()
-    plt.plot(receiver)
+    peak_corr(pulse, receiver, 'sine (rectangle window)')
+    # triangle windowed sinusoid
+    pulse = triangular_pulse(L,freq)
+    receiver, _ = H(pulse,N,spread,noise_power, targets=delays)
+    peak_corr(pulse, receiver, 'sine (triangle window)')
+    # hamming windowed sinusoid
+    pulse = hamming_pulse(L,freq)
+    receiver, _ = H(pulse,N,spread,noise_power, targets=delays)
+    peak_corr(pulse, receiver, 'sine (hamming window)')
+    # rectangle windowed sinc
+    pulse = sinc_pulse(L, freq)
+    receiver, _ = H(pulse,N,spread,noise_power, targets=delays)
+    peak_corr(pulse, receiver, 'sinc')
+    # rectangle windowed flattened sinc
+    pulse = warped_sinc_pulse(L, freq, 4)
+    receiver, _ = H(pulse,N,spread,noise_power, targets=delays)
+    peak_corr(pulse, receiver, 'flattened sinc')
     print(f'Ground Truth Delays: {delays}')
-    peak_corr(pulse, receiver)
-
+    # comparison between original sinc and flattened sinc
+    plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    plt.plot(sinc_pulse(L, freq))
+    plt.title('sinc pulse')
+    plt.subplot(122)
+    plt.plot(warped_sinc_pulse(L, freq, 4))
+    plt.title('flattened sinc')
     plt.show()
