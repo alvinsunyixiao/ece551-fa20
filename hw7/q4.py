14853cf9dcf8711af623282f60a9dd893eb1f981
Alvin Sun <alvinsunyixiao@gmail.com>
2020-10-15 15:23:55 -0500

    rework q4

--- hw7/q4.py
+++ hw7/q4.py
@@ -4,7 +4,6 @@ import numpy as np
 
 from scipy import signal
 from scipy.io import wavfile
-from scipy.linalg import solve_circulant, circulant
 from tqdm import trange
 
 def parse_args():
@@ -30,8 +29,12 @@ if __name__ == '__main__':
     # construct Yule-Walker matrix index
     i, j = np.meshgrid(np.arange(args.order), np.arange(args.order), indexing='ij')
     YW_idx = np.abs(i - j)
+    # pitch phase tracking
+    phase_last = 0
+    # minimum pitch index @ 250Hz
+    p_min = fs // 250
     # solve for each frame
-    for i in trange(num_frames):
+    for i in trange(0, num_frames):
         win_data = data[i*frame_size: (i+1)*frame_size]
         win_data_pad = np.pad(win_data, [0, args.order])
         win_corr = np.correlate(win_data_pad, win_data, 'valid')
@@ -43,10 +46,27 @@ if __name__ == '__main__':
         # estimate for error / excitation signal
         e = win_data - win_data_pred
         data_err[i*frame_size: (i+1)*frame_size] = e
+        # voice / unvoiced detection
+        win_corr_full = np.correlate(np.pad(win_data, [0, win_data.shape[0]]), win_data, 'valid')
+        corr_0 = win_corr_full[0]
+        win_corr_full[:p_min] = 0
+        pitch_idx = np.argmax(win_corr_full)
         # synthesis
-        y_hat = signal.lfilter([1.], np.hstack([[1.], -alpha]), np.random.randn(win_data.shape[0]) * e.std())
+        if win_corr_full[pitch_idx] / corr_0 > .25: # voiced frame
+            excitation = np.zeros_like(e)
+            pulse_idx = (phase_last + pitch_idx) % frame_size
+            while True:
+                excitation[pulse_idx] = 1
+                phase_last = pulse_idx
+                pulse_idx += pitch_idx
+                if pulse_idx >= frame_size:
+                    break
+        else: # unvoiced frame
+            excitation = np.random.randn(e.shape[0])
+        # normalize with energy
+        excitation = excitation * np.sqrt(np.sum(np.square(e)) / np.sum(np.square(excitation)))
+        # sythesis
+        y_hat = signal.lfilter([1.], np.hstack([[1.], -alpha]), excitation)
         data_synth[i*frame_size: (i+1)*frame_size] = y_hat
 
-    plt.plot(data_synth - data)
-    plt.show()
     wavfile.write(args.output, fs, data_synth)
