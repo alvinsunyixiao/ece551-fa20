071eab3e55533d4bc9dc8d5e76cbcf5898e9e68a
Alvin Sun <alvinsunyixiao@gmail.com>
2020-10-15 10:51:16 -0500

    update

--- hw7/q4.py
+++ hw7/q4.py
@@ -16,23 +16,20 @@ def parse_args():
     parser.add_argument('--window', type=float, default=10e-3,
                         help='window duration in [s]')
     parser.add_argument('-o', '--output', type=str, default='out.wav',
-                        help='path to store the output wav file')
+                        help='path to store the output reconstructed audio (Default to out.wav)')
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
     fs, data = wavfile.read(args.input)
-    data = data.astype(np.float) / (1 << 15)
+    data = data.astype(np.float32) / (1 << 15)
     data_synth = np.zeros_like(data)
-    # pad 0s in front of the data for performing linear prediction
-    data_pad_left = np.pad(data, [args.order, 0])
+    data_err = np.zeros_like(data)
     frame_size = int(fs * args.window)
     num_frames = data.shape[0] // frame_size
     # construct Yule-Walker matrix index
     i, j = np.meshgrid(np.arange(args.order), np.arange(args.order), indexing='ij')
     YW_idx = np.abs(i - j)
-    # error / excitation signal
-    data_err = np.zeros_like(data)
     # solve for each frame
     for i in trange(num_frames):
         win_data = data[i*frame_size: (i+1)*frame_size]
@@ -42,11 +39,14 @@ if __name__ == '__main__':
         alpha = np.linalg.solve(win_corr[YW_idx], win_corr[1:])
         # linear prediction
         win_data_left = np.pad(win_data, [args.order, 0])
-        win_data_left = data_pad_left[i*frame_size: (i+1)*frame_size + args.order]
         win_data_pred = np.convolve(win_data_left[:-1], alpha, 'valid')
-        # calculate error signal
-        data_err[i*frame_size: (i+1)*frame_size] = win_data - win_data_pred
+        # estimate for error / excitation signal
+        e = win_data - win_data_pred
+        data_err[i*frame_size: (i+1)*frame_size] = e
         # synthesis
-        data_synth[i*frame_size: (i+1)*frame_size] = signal.lfilter([1.], np.hstack([[1.], -alpha]), win_data - win_data_pred)
+        y_hat = signal.lfilter([1.], np.hstack([[1.], -alpha]), np.random.randn(win_data.shape[0]) * e.std())
+        data_synth[i*frame_size: (i+1)*frame_size] = y_hat
 
+    plt.plot(data_synth - data)
+    plt.show()
     wavfile.write(args.output, fs, data_synth)
